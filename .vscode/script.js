8755a77fa054de020c870b2f8f6a74cbb04dbcb5
Edimilson <edimilsommurer@gmail.com>
2023-12-26 12:29:55 -0300

    add readme

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,10 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+    html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute("src", "./assets/avatarp.png")
   } else {
     img.setAttribute("src", "./assets/avatarp.png")
   }
